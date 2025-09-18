56271099ecb2a06f36405d36bf62e5fc7a2af111
Lorenzo Rota <lorenzokaide@gmail.com>
2025-09-18 09:53:40 +0200

    Added calculations for example 2

--- example_probabilistic1.py
+++ example_probabilistic1.py
@@ -84,5 +84,47 @@ def latex_probabilities_table(points, centers):
     print(r"\end{table}")
     print()
 
-latex_distances_table(points, centers)
-latex_probabilities_table(points, centers)
+
+def latex_jdf_table(points, centers):
+    print(r"\begin{table}[h!]")
+    print(r"\centering")
+    print(r"\begin{tabular}{c|cccc|c}")
+    print(r"\textbf{Point} & $d_1 d_2 + d_1 d_3 + d_2 d_3$ & $D(\mathbf{x})$ \\")
+    print(r"\hline")
+    
+    total_jdf = 0
+    
+    for i, point in enumerate(points):
+        distances = [np.linalg.norm(point - center) for center in centers]
+        d1, d2, d3 = distances
+        
+        # Calculate numerator (product of all distances)
+        numerator = d1 * d2 * d3
+        
+        # Calculate denominator (sum of leave-one-out products)
+        denominator = d1*d2 + d1*d3 + d2*d3
+        
+        # Calculate D(x) for this point
+        d_x = numerator / denominator
+        total_jdf += d_x
+        
+        print(f"$\\mathbf{{x}}_{{{i+1}}}$ & {denominator:.2f} & {d_x:.3f} \\\\")
+    
+    print(r"\hline")
+    print(f"\\multicolumn{{2}}{{r|}}{{\\textbf{{Total JDF:}}}} & \\textbf{{{total_jdf:.3f}}} \\\\")
+    print(r"\end{tabular}")
+    print(r"\caption{Joint Distance Function (JDF) computation for each data point. $D(\mathbf{x}) = \frac{d_1 d_2 d_3}{d_1 d_2 + d_1 d_3 + d_2 d_3}$.}")
+    print(r"\label{tab:jdf}")
+    print(r"\end{table}")
+    print()
+
+
+def main():
+    # Example 1
+    latex_distances_table(points, centers)
+    latex_probabilities_table(points, centers)
+
+    # Example 2
+    latex_jdf_table(points, centers)
+    
+main()
